8b8bc472e422453e5090c38f13a623fbb0baa576
RahulPolubothu <80740963+rahul061101@users.noreply.github.com>
2021-05-24 12:00:42 +0530

    Add files via upload
    Day-2

---
+++ format string.py
@@ -0,0 +1,11 @@
+name="Sachin Tendulkar"
+age=48
+print("my name is {}. \n i am of age {} years old".format(name,age))
+
+Name="ms dhoni"
+Age=39
+print("My Name is {}. \n I am of Age {} years old".format(Name,Age))
+ 
+print("Avengers World")
+print("Avengers\nWorld")
+
